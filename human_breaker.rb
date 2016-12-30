a592abab84f1830e686205b03f4780336d793660
Eliott Ham <eliotthham@gmail.com>
2016-12-29 21:24:01 -0800

    Created method for human codebreaker to guess 4 colors per turn.

--- human_breaker.rb
+++ human_breaker.rb
@@ -3,5 +3,17 @@ class HumanBreaker
 		puts "Color options: 'B' - blue, 'G' - green, 'R' - red, 'P' - purple, 'O' - orange, 'W' - white."
 		choices = Array.new(4)
 		4.times do |i|
-			puts "Choose a color for hole #{i + 1}."
+			puts "Choose a color for slot #{i + 1}."
 			choices[i] = gets.chomp
+			if (choices[i] =~ /[B || G || R || P || O || W]/) == nil
+				puts "Invalid choice. Try again."
+				redo
+			end
+		end
+		puts "You entered: #{choices[0]} #{choices[1]} #{choices[2]} #{choices[3]}."
+		return choices
+	end
+end
+
+me = HumanBreaker.new
+me.make_guess
